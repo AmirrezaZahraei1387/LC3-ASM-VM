b91d1a17077c136a70da3a92dd2f452a69d36238
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-10-22 20:18:13 -0600

    opcodes added

---
+++ data/opcodes.hpp
@@ -0,0 +1,25 @@
+//
+// Created by KAVOSH on 10/22/2023.
+//
+#ifndef LC3_ASM_VM_OPCODES_HPP
+#define LC3_ASM_VM_OPCODES_HPP
+enum
+{
+    OP_BR = 0, // branch
+    OP_ADD,    // add
+    OP_LD,     // load
+    OP_ST,     // store
+    OP_JSR,    // jump register
+    OP_AND,    // bitwise and
+    OP_LDR,    // load register
+    OP_STR,    // store register
+    OP_RTI,    // unused
+    OP_NOT,    // bitwise not
+    OP_LDI,    // load indirect
+    OP_STI,    // store indirect
+    OP_JMP,    // jump
+    OP_RES,    // reserved (unused)
+    OP_LEA,    // load effective address
+    OP_TRAP    // execute trap
+};
+#endif //LC3_ASM_VM_OPCODES_HPP
