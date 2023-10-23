11957dfdd7cbfa6345cc19d0e5e3b3471932b0b3
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-10-22 20:13:30 -0600

    the memery file to similate the memory memory needed for LC3 created.

---
+++ data/memory.h
@@ -0,0 +1,8 @@
+//
+// Created by KAVOSH on 10/22/2023.
+//
+#ifndef LC3_ASM_VM_MEMORY_H
+#define LC3_ASM_VM_MEMORY_H
+#define MEMORY_MAX (1 << 16)
+uint16_t memory[MEMORY_MAX];  // 65536 locations
+#endif //LC3_ASM_VM_MEMORY_H
