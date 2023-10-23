aae6132fb5bb4441714087f9bc6d3c09c124550b
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-10-22 20:30:12 -0600

    the main loop of VM with setup and shut down created.

--- main.c
+++ main.c
@@ -1,6 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 
-int main() {
-    printf("Hello, World!\n");
+#include "data/regisiter.h"
+#include "data/opcodes.h"
+#include "data/confl.h"
+
+
+int main(int argc, const char* argv[]) {
+    if (argc < 2)
+    {
+        /* show usage string */
+        printf("lc3 [image-file1] ...\n");
+        exit(2);
+    }
+
+    for (int j = 1; j < argc; ++j)
+    {
+        if (!read_image(argv[j]))
+        {
+            printf("failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
+
+    /* since exactly one condition flag should be set at any given time, set the Z flag */
+    reg[R_COND] = FL_ZRO;
+    /* set the PC to starting position */
+    /* 0x3000 is the default */
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+
+    while (running)
+    {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op)
+        {
+            case OP_ADD:
+
+                break;
+            case OP_AND:
+
+                break;
+            case OP_NOT:
+
+                break;
+            case OP_BR:
+
+                break;
+            case OP_JMP:
+
+                break;
+            case OP_JSR:
+
+                break;
+            case OP_LD:
+
+                break;
+            case OP_LDI:
+
+                break;
+            case OP_LDR:
+
+                break;
+            case OP_LEA:
+
+                break;
+            case OP_ST:
+
+                break;
+            case OP_STI:
+
+                break;
+            case OP_STR:
+
+                break;
+            case OP_TRAP:
+
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+
+                break;
+        }
+    }
+
+    restore_input_buffering();
     return 0;
 }
